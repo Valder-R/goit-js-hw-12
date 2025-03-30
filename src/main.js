335667065a3be257a5d49e45dee4c252fac042bc
Volodymyr_Rostovtsev <fortik1510@gmail.com>
2025-03-30 01:06:56 +0100

    main improvements

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ const form = document.querySelector(".form");
 let searchValue;
 let pageNumber = 1;
     
-form.addEventListener("submit", ev => {
+form.addEventListener("submit", async (ev) => {
     ev.preventDefault();
     searchValue = inputField.value.trim();
     inputField.value = "";
@@ -20,21 +20,25 @@ form.addEventListener("submit", ev => {
         clearGallery();
         showLoader();
         pageNumber = 1;
-        getImagesByQuery(searchValue, pageNumber)
-            .then(data => {
-                hideLoader();
-                addImages(data.hits, pageNumber);
-                showLoadMoreButton()
-            })
-            .catch(error => {
-                hideLoader();
+        try {
+            const data = await getImagesByQuery(searchValue, pageNumber);
+            addImages(data.hits, pageNumber);
+            if (Math.ceil(data.totalHits / 15) == pageNumber) {
+                throw new Error("We're sorry, but you've reached the end of search results.")
+            }
+            showLoadMoreButton()
+            }
+            catch (error) {
                 iziToast.show({
-                        title: 'Error',
-                        message: error.message,
-                        color: 'red',
-                        position: "topRight"
-                    });
-    });
+                    title: 'Error',
+                    message: error.message,
+                    color: 'red',
+                    position: "topRight"
+                });
+            }
+            finally{
+                hideLoader();
+            };
     } else {
         iziToast.show({
                         title: 'Error',
@@ -46,34 +50,35 @@ form.addEventListener("submit", ev => {
 });
 
 const loadMoreButton = document.querySelector("button[type='button']");
-loadMoreButton.addEventListener("click", ev => {
+loadMoreButton.addEventListener("click", async (ev) => {
     ev.preventDefault();
     pageNumber += 1;
     hideLoadMoreButton();
     showLoader();
-    getImagesByQuery(searchValue, pageNumber)
-        .then(data => {
-            hideLoader();
-            addImages(data.hits, pageNumber);
-            const galleryItem = document.querySelector(".gallery-item");
-            const rectOfGalleryItem = galleryItem.getBoundingClientRect();
-            window.scrollBy({
-                top: rectOfGalleryItem["height"]*2,
-                left: 0,
-                behavior: "smooth",
-                });
-            if (Math.ceil(data.totalHits/15) == pageNumber) {
-                throw new Error("We're sorry, but you've reached the end of search results.")
-            }
-            showLoadMoreButton()
-        })
-        .catch(error => {
-            hideLoader();
+    try {
+        const data = await getImagesByQuery(searchValue, pageNumber);
+        addImages(data.hits, pageNumber);
+        const galleryItem = document.querySelector(".gallery-item");
+        const rectOfGalleryItem = galleryItem.getBoundingClientRect();
+        window.scrollBy({
+            top: rectOfGalleryItem["height"] * 2,
+            left: 0,
+            behavior: "smooth",
+        });
+        if (Math.ceil(data.totalHits / 15) == pageNumber) {
+            throw new Error("We're sorry, but you've reached the end of search results.")
+        }
+        showLoadMoreButton()
+        }
+        catch (error){
             iziToast.show({
-                    title: 'Error',
-                    message: error.message,
-                    color: 'red',
-                    position: "topRight"
-                });
-    });
+                title: 'Error',
+                message: error.message,
+                color: 'red',
+                position: "topRight"
+            });
+        }
+        finally{
+            hideLoader();
+        };
 })
